0d4ec09ac890be7ca8f6767ab63cf2c9b11abc6a
PietroSpalluto <70751487+PietroSpalluto@users.noreply.github.com>
2023-05-30 19:56:35 +0200

    Add files via upload

---
+++ links.py
@@ -0,0 +1,3 @@
+# https://medium.com/mlearning-ai/quantum-data-and-its-embeddings-1-3b022b2f1245
+# https://www.youtube.com/playlist?list=PLOFEBzvs-VvqJwybFxkTiDzhf5E11p8BI
+# https://learn.qiskit.org/course/machine-learning/introduction
